702bcd14c8da52f15ef7760439925934733ab7c8
Arjun Variar <accio.arjun@gmail.com>
2015-04-15 16:36:26 +0530

    Adding crossover function to the mix.
    Building the genetic routine library.

--- geneticrout.js
+++ geneticrout.js
@@ -9,9 +9,50 @@ function select(sumfiteness, population)
     var j = 0;
     var rand = randrout.random() * sumfiteness;
     do {
-        partsum = population[j].fitness;
+        partsum += population[j].fitness;
         j++;
     }
-    while ((partsum >= rand) || (j == population.length))
+    while (!((partsum >= rand) || (j == population.length)))
     return j;
+}
+
+/*
+ * Crossover the 2 parents and return the crossed children for the new generation.
+ */
+function crossover(parent1, parent2, child1, child2, lchrom,
+    ncross, nmutation, jcross, pcross, pmutation)
+{
+    var j = 0;
+    if (randrout.flip(pcross))
+    {
+        jcross = randrout.rnd(0, lchrom - 2);
+        ncross++;
+    }
+    else
+    {
+        jcross = lchrom;
+    }
+
+    for (var i = 0; i < jcross; i++)
+    {
+        child1[j] = mutation(parent1[j], pmutation, nmutation);
+        child2[j] = mutation(parent2[j], pmutation, nmutation);
+    }
+
+    if (jcross != lchrom)
+    {
+        for (var j = jcross + 1; j < lchrom; j++)
+        {
+            child1[j] = mutation(parent2[j], pmutation, nmutation);
+            child2[j] = mutation(parent1[j], pmutation, nmutation);
+        }
+    }
+}
+
+/*
+ * Build the mutation function.
+ */
+function mutation(alleleval, pmutation, nmutation)
+{
+
 }
