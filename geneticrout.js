81915f25581035275b826ea8ebb338600bdbf245
Arjun Variar <accio.arjun@gmail.com>
2015-04-16 14:42:25 +0530

    Generations.
    Building the logic to generate new generation of likely candidates.

--- geneticrout.js
+++ geneticrout.js
@@ -1,9 +1,10 @@
 var randrout = require('./randchoicerout');
+var approut = require('./applicationroutines');
 
 /*
  * Function which returns which individual to select from a population.
  */
-function select(sumfiteness, population)
+function select(popsize, sumfiteness, population)
 {
     var partsum = 0;
     var j = 0;
@@ -12,7 +13,7 @@ function select(sumfiteness, population)
         partsum += population[j].fitness;
         j++;
     }
-    while (!((partsum >= rand) || (j == population.length)))
+    while (!((partsum >= rand) || (j == popsize)))
     return j;
 }
 
@@ -62,4 +63,28 @@ function mutation(alleleval, pmutation, nmutation)
         mutatedbit = alleleval == 1 ? 0 : 1;
     }
     return mutatedbit;
+}
+
+/*
+ * Get a new generation of population from the older one.
+ */
+function generation(oldpop, newpop, popsize, sumfiteness, lchrom, ncross, nmutation, jcross, pcross, pmutation)
+{
+    var j, mate1, mate2;
+    j = 0;
+    do {
+        mate1 = select(popsize, sumfiteness, oldpop);
+        mate2 = select(popsize, sumfiteness, oldpop);
+        crossover(oldpop[mate1].chrom, oldpop[mate2].chrom, newpop[j].chrom, newpop[j].chrom, lchrom, ncross, nmutation, jcross, pcross, pmutation);
+        jChild = newpop[j];
+        j1Child = newpop[j + 1];
+        jChild.x = approut.decode(jChild.chrom, lchrom);
+        j1Child.x = approut.decode(j1Child.chrom, lchrom);
+        jChild.fitness = approut.objfunc(jChild.x);
+        j1Child.fitness = approut.objfunc(j1Child.x);
+        jChild.parent1 = j1Child.parent1 = mate1;
+        jChild.parent2 = j1Child.parent2 = mate2;
+        jChild.xsite = j1Child.xsite = jcross;
+        j += 2;
+    } while (j < popsize)
 }
